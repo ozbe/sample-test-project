2712280ee2d51e4a3505d36b351d6837b6e822d7
ozbe <1372945+ozbe@users.noreply.github.com>
2020-09-01 09:33:43 -0400

    refactor Build

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using Unity.Simulation.Games;
 using System;
 using System.IO.Compression;
 using UnityEngine.Networking;
@@ -7,6 +6,9 @@ using System.IO;
 
 #if true || UNITY_CLOUD_BUILD
 public class Build {
+    const string GAME_SIMULATION_API_HOST = "https://api.prd.gamesimulation.unity3d.com";
+    const string UNITY_API_HOST = "https://api.unity.com";
+
     public static void PostExport(string exportPath)
     {
         Debug.Log(string.Format("Export path: {0}", exportPath));
@@ -16,81 +18,88 @@ public class Build {
         var username = Environment.GetEnvironmentVariable("UNITY_USERNAME");
         var password = Environment.GetEnvironmentVariable("UNITY_PASSWORD");
 
-        // zip
         Debug.Log("Zipping...");
-        var zipPath = Path.Combine(Path.GetTempPath(), "upload.zip");
+        string zipPath = ZipBuild(exportPath);
+        Debug.Log("Done.");
+
         Debug.Log(string.Format("Zip path: {0}", zipPath));
-        ZipFile.CreateFromDirectory(Path.GetDirectoryName(exportPath), zipPath);
-        Debug.Log("Done.");
 
-        // get token
         Debug.Log("Getting token...");
-        string accessToken;
-        var loginRequest = new LoginRequest(username, password);
-        var loginJson = JsonUtility.ToJson(loginRequest);
-        using (var request = UnityWebRequest.Post("https://api.unity.com/v1/core/api/login", loginJson))
-        {
-            request.SetRequestHeader("Content-Type", "application/json");
-            request.SetRequestHeader("Accept", "application/json");
-
-            Execute(request);
-            if (request.isNetworkError || request.isHttpError)
-            {
-                Debug.LogError(request.error);
-                return;
-            }
-
-            var response = request.downloadHandler.text;
-            var loginResponse = JsonUtility.FromJson<LoginResponse>(response);
-            accessToken = loginResponse.access_token;
-        }
+        string accessToken = GetAccessToken(username, password);
         Debug.Log("Done.");
 
-        // get upload path
         Debug.Log("Creating Game Simulation build...");
-        BuildResponse buildResponse;
-        var buildsUrl = string.Format("https://api.prd.gamesimulation.unity3d.com/v1/builds?projectId={0}", unityProjectId);
-        var buildRequest = new BuildRequest("SampleTestProject", "");
-        var buildJson = JsonUtility.ToJson(buildRequest);
-        using (var request = UnityWebRequest.Post(buildsUrl, buildJson))
-        {
-            request.SetRequestHeader("Content-Type", "application/json");
-            request.SetRequestHeader("Accept", "application/json");
-            request.SetRequestHeader("Authorization", string.Format("Bearer {0}", accessToken));
-
-            Execute(request);
-            if (request.isNetworkError || request.isHttpError)
-            {
-                Debug.LogError(request.error);
-                return;
-            }
-
-            var response = request.downloadHandler.text;
-            buildResponse = JsonUtility.FromJson<BuildResponse>(response);
-        }
+        BuildResponse buildResponse = CreateGameSimulationBuild(unityProjectId, accessToken);
         Debug.Log("Done.");
-
 
         Debug.Log(string.Format("Build upload: {0}", buildResponse.id));
 
-        // upload build
         Debug.Log("Uploading Game Simulation build...");
+        UploadGameSimulationBuild(zipPath, buildResponse);
+        Debug.Log("Done.");
+
+        Debug.Log("Creating Game Simulation...");
+        var simulationResponse = CreateGameSimulation(unityProjectId, accessToken, buildResponse);
+        Debug.Log("Done.");
+
+        Debug.Log(string.Format("Simulation id: {0}", simulationResponse.id));
+}
+
+    private static string ZipBuild(string exportPath)
+    {
+        var zipPath = Path.Combine(Path.GetTempPath(), "upload.zip");
+        ZipFile.CreateFromDirectory(Path.GetDirectoryName(exportPath), zipPath);
+        return zipPath;
+    }
+
+    private static string GetAccessToken(string username, string password)
+    {
+        var loginUrl = string.Format("{0}/v1/core/api/login", UNITY_API_HOST);
+
+        var loginRequest = new LoginRequest(username, password);
+        var loginJson = JsonUtility.ToJson(loginRequest);
+
+        using (var request = UnityWebRequest.Post(loginUrl, loginJson))
+        {
+            Json(request);
+
+            var loginResponse = Execute<LoginResponse>(request);
+           
+            return loginResponse.access_token;
+        }
+    }
+
+    private static BuildResponse CreateGameSimulationBuild(string unityProjectId, string accessToken)
+    {
+        var buildsUrl = string.Format("{0}/v1/builds?projectId={1}", GAME_SIMULATION_API_HOST, unityProjectId);
+
+        var buildRequest = new BuildRequest("SampleTestProject", "");
+        var buildJson = JsonUtility.ToJson(buildRequest);
+
+        using (var request = UnityWebRequest.Post(buildsUrl, buildJson))
+        {
+            Json(request);
+            Auth(request, accessToken);
+
+            return Execute<BuildResponse>(request);
+        }
+
+    }
+
+    private static void UploadGameSimulationBuild(string zipPath, BuildResponse buildResponse)
+    {
         using (var request = new UnityWebRequest(buildResponse.upload_uri, UnityWebRequest.kHttpVerbPOST))
         {
             request.uploadHandler = new UploadHandlerFile(zipPath);
 
             Execute(request);
-            if (request.isNetworkError || request.isHttpError)
-            {
-                Debug.LogError(request.error);
-                return;
-            }
-
         }
-        Debug.Log("Done.");
+    }
 
-        Debug.Log("Creating Game Simulation...");
-        var simulationsUrl = string.Format("https://api.prd.gamesimulation.unity3d.com/v1/jobs?projectId={0}", unityProjectId);
+    private static SimulationResponse CreateGameSimulation(string unityProjectId, string accessToken, BuildResponse buildResponse)
+    {
+        var simulationsUrl = string.Format("{0}/v1/jobs?projectId={1}", GAME_SIMULATION_API_HOST, unityProjectId);
+
         // TODO - load request from json asset
         var simulationRequest = new SimulationRequest();
         simulationRequest.jobName = "Sample Test Project Simulation";
@@ -105,32 +114,47 @@ public class Build {
         simulationRequest.maxRuntimeSeconds = "300";
         simulationRequest.runsPerParamCombo = 10;
         var simulationJson = JsonUtility.ToJson(simulationRequest);
+
         using (var request = UnityWebRequest.Post(simulationsUrl, simulationJson))
         {
-            request.SetRequestHeader("Content-Type", "application/json");
-            request.SetRequestHeader("Accept", "application/json");
-            request.SetRequestHeader("Authorization", string.Format("Bearer {0}", accessToken));
+            Json(request);
+            Auth(request, accessToken);
 
-            Execute(request);
-            if (request.isNetworkError || request.isHttpError)
-            {
-                Debug.LogError(request.error);
-                return;
-            }
-
-            var response = request.downloadHandler.text;
-            var simulationResponse = JsonUtility.FromJson<SimulationResponse>(response);
-            Debug.Log(string.Format("Simulation id: {0}", simulationResponse.id));
+            return Execute<SimulationResponse>(request);
         }
-        Debug.Log("Done.");
+    }
+
+    public static void Json(UnityWebRequest request)
+    {
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Accept", "application/json");
+    }
+
+    public static void Auth(UnityWebRequest request, string accessToken)
+    {
+        request.SetRequestHeader("Authorization", string.Format("Bearer {0}", accessToken));
+    }
+
+    public static T Execute<T>(UnityWebRequest request)
+    {
+        Execute(request);
+        var response = request.downloadHandler.text;
+        return JsonUtility.FromJson<T>(response);
     }
 
     public static void Execute(UnityWebRequest request) 
     {
         request.SendWebRequest();
+
+        // HACK
         while (request.downloadProgress < 1.0f)
         {
             System.Threading.Thread.Sleep(100);
+        }
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            throw new Exception(request.error);
         }
     }
 
