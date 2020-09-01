254db14495ceebf8cefcd241ed082df1ee99bf5d
ozbe <1372945+ozbe@users.noreply.github.com>
2020-08-31 21:24:43 -0400

    PostExport

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -3,6 +3,108 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Simulation.Games;
 using System;
+using System.IO.Compression;
+using UnityEngine.Networking;
+using System.IO;
+
+#if true || UNITY_CLOUD_BUILD
+public class Build {
+    public static void PostExport(string exportPath)
+    {
+        var unityProjectId = Environment.GetEnvironmentVariable("UNITY_PROJECT_ID");
+        var username = Environment.GetEnvironmentVariable("UNITY_USERNAME");
+        var password = Environment.GetEnvironmentVariable("UNITY_PASSWORD");
+
+        // zip
+        var zipPath = Path.Combine(Path.GetDirectoryName(exportPath), "upload.zip");
+        ZipFile.CreateFromDirectory(exportPath, zipPath);
+
+        // get token
+        string accessToken;
+        var loginRequest = new LoginRequest(username, password);
+        var loginJson = JsonUtility.ToJson(loginRequest);
+        using (var request = UnityWebRequest.Post("https://api.unity.com/v1/core/api/login", loginJson))
+        {
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Accept", "application/json");
+            request.Send();
+            var response = System.Text.Encoding.UTF8.GetString(request.downloadHandler.data);
+            var loginResponse = JsonUtility.FromJson<LoginResponse>(response);
+            accessToken = loginResponse.access_token;
+        }
+
+        // get upload path
+        BuildResponse buildResponse;
+        var buildsUrl = string.Format('https://api.prd.gamesimulation.unity3d.com/v1/builds?projectId={0}', unityProjectId);
+        var buildRequest = new BuildRequest("Foo", "Bar");
+        var buildJson = JsonUtility.ToJson(buildRequest);
+        using (var request = UnityWebRequest.Post(buildsUrl, buildJson))
+        {
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Accept", "application/json");
+            request.SetRequestHeader("Authorization", string.Format("Bearer {0}", accessToken));
+            request.Send();
+            var response = System.Text.Encoding.UTF8.GetString(request.downloadHandler.data);
+            buildResponse = JsonUtility.FromJson<BuildResponse>(response);
+        }
+
+        Debug.Log(string.Format("Build upload: {0}", buildResponse.id));
+
+        // upload build
+        using (var request = new UnityWebRequest(buildResponse.upload_uri, UnityWebRequest.kHttpVerbPOST))
+        {
+            request.uploadHandler = new UploadHandlerFile(zipPath);
+            request.Send();
+            if (request.isNetworkError || request.isHttpError)
+                Debug.LogError(request.error);
+            else
+            {
+                Debug.Log("Success!");
+            }
+
+        }
+    }
+
+    [Serializable]
+    struct LoginRequest
+    {
+        public LoginRequest(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+            this.grant_type = "PASSWORD";
+        }
+        public string username;
+        public string password;
+        public string grant_type;
+    }
+
+    [Serializable]
+    struct LoginResponse
+    {
+        public string access_token;
+    }
+
+    [Serializable]
+    struct BuildRequest 
+    {
+        public BuildRequest(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+        public string name;
+        public string description;
+    }
+
+    [Serializable]
+    struct BuildResponse
+    {
+        public string id;
+        public string upload_uri;
+    }
+}
+#endif
 
 public class GameController : MonoBehaviour
 {
